dac6b1757e1330ccf108af45aae1d7c1d030f9e1
WillyD <sopwill3@gmail.com>
2019-05-08 09:01:02 -0600

    added dotenv

--- giphy-react/src/MainComponent/MainComponent.jsx
+++ giphy-react/src/MainComponent/MainComponent.jsx
@@ -14,15 +14,13 @@ function MainComponent() {
   const [foundGifs, setFoundGifs] = useState([]);
   const [randomGif, setRandomGif] = useState('');
 
-  // couldn't figure out .env with React
-  const giphyKey = 'XMi5Q4guHITm8b07dJQhwWhVVdnZe6MW'
 
   useEffect(()=>{
     getRandomGif()
   }, [foundGifs]);
 
   const getRandomGif = async () => {
-    const randomGif = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${giphyKey}&limit=5`);
+    const randomGif = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=5`);
 
     const parsedGif = await randomGif.json();
 
@@ -30,14 +28,13 @@ function MainComponent() {
     // Allow me to acces nested layers of the object!
         // parsedGif.data.url came up 'cant read prop of undefined'...
         // yet const gif = parsedGif.data; gif.url worked
-        // WHY??
 
     setRandomGif(parsedGif.data);
   }
 
   const createGifsArray = async (searchTerm) => {
 
-    const gottenGifs = await fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${giphyKey}&limit=10`);
+    const gottenGifs = await fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=10`);
 
     const parsedGifs = await gottenGifs.json();
 
@@ -50,7 +47,7 @@ function MainComponent() {
     const gifsList = gifs.map((gif) => {
       return (
         <li key={gif.id}>
-          <iframe src={gif.embed_url}/>
+          <iframe title={gif.id} src={gif.embed_url}/>
         </li>
       );
     });
@@ -63,7 +60,7 @@ function MainComponent() {
 
   return (<div>
     <h1 style={headerStyle}>Giphy 'app'</h1>
-    <iframe src={randomGif.embed_url} />
+    <iframe title={randomGif.id} src={randomGif.embed_url} />
     <SearchForm createGifsArray={createGifsArray}/>
     <GifIndex foundGifs={foundGifs}/>
   </div>);
