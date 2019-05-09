786b66cf1ba96169ac99cdff46cbdf15c3c2b77a
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-05-08 21:05:54 -0600

    had to fix my git repo, almost had a heart attack

---
+++ homework/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import GifContainer from './GifContainer/GifContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gifs for the soul.</h1>
+      <GifContainer />
+    </div>
+  );
+}
+
+export default App;
